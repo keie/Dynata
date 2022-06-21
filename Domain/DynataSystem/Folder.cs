23ab6757fcd5afb1e73b40df473d1ba954b6026b
_k3v1n <kevin.nava@externos-cl.cencosud.com>
2022-06-20 23:19:52 -0400

    INITIAL COMMIT

---
+++ Domain/DynataSystem/Folder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DynataSystem
+{
+    public class Folder
+    {
+        public int Id { get; set; } 
+        public string? FolderName { get; set; }
+        public float? Size { get; set; }
+        public bool isSubFolder { get; set; }
+
+    }
+}
